5546f23f02146354da140d57d01b53d8d2f349b9
Woodward Spivey <Woodward Spivey>
2024-05-25 16:06:16 -0400

    make homepage use it's own html page instead of just base.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render 
 
 def index(request):
-    return render(request, 'base.html', {})
+    return render(request, 'home/home.html', {'username': request.user.username})
 
