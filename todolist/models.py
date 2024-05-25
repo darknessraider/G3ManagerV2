26660cbd7e685091b68bf3026733904c24e84898
Woodward Spivey <Woodward Spivey>
2024-05-25 12:01:01 -0400

    add str function to todo item and add creator field to todoitem

--- todolist/models.py
+++ todolist/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TodoItem(models.Model):
     name = models.CharField(max_length=50)
     creation_time_stamp = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     
+    def __str__(self):
+        return self.name
 
  
