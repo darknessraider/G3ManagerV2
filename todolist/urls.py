b7084ee64dc5958445222111c6b863bd26bc61d2
Woodward Spivey <Woodward Spivey>
2024-06-14 19:34:59 -0400

    item create view and change around navbar

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>', views.ItemView.as_view(), name='home_page'),
+    path('<int:id>', views.ItemView.as_view(), name='item_view'),
+    path('create_todo_item', views.item_create_view, name='item_create_view'),
 ]
+
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 from .models import TodoItem
+from .forms import CreateTodoitemForm
 
 class ItemView(TemplateView):
     template_name = 'item.html'
@@ -10,4 +12,20 @@ class ItemView(TemplateView):
         context['item'] = TodoItem.objects.get(kwargs['id'])
         return context
 
+
+def item_create_view(request):
+    if request.method != 'POST':
+        form = CreateTodoitemForm() 
+        return render(request, 'genericform.html', {'form': form})
+    
+    form = CreateTodoitemForm(request.POST)
+
+    if not form.is_valid():
+        return HttpResponse('<h1>Error With Form Validation</h1>')
+
+    data = form.cleaned_data
+
+    item = TodoItem(name=data['name'], creator=request.user)
+    item.save()
+    return HttpResponseRedirect('') 
 
